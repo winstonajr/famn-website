ecac849d916bb42ce67b191425f6e8900bce878f
AllanFGR <115052566+Allan-Figueiredo@users.noreply.github.com>
2025-08-13 13:56:57 -0300

    Hero - Primeira versão

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,16 @@
+import Hero from "@/components/Hero";
+
 export default function Home() {
   return (
     <main>
-      <h1>Hello</h1>
+      <Hero
+        title="Bem-vindo ao FAMN"
+        subtitle="Fundação fundo de amparo à míseria nacional"
+        description="Solidariedade, humanismo e cooperação"
+        buttonText="Saiba Mais"
+        buttonLink="/sobre"
+        backgroundImage="/hero-image.jpg"
+      />
     </main>
   );
 }
